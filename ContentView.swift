a17618dde60fd7b77ca31fa3dcc139960a5212c4
parksungsoo <72736657+sunshiningsoo@users.noreply.github.com>
2022-04-16 09:00:43 +0900

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDictionaryApp.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
