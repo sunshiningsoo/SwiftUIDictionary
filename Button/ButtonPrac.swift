3a94b1c8e988af5d97bb86fa5b3f695385175c58
parksungsoo <72736657+sunshiningsoo@users.noreply.github.com>
2022-04-17 16:13:19 +0900

    button add

---
+++ Button/ButtonPrac.swift
@@ -0,0 +1,24 @@
+//
+//  ButtonPrac.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/17.
+//
+
+import SwiftUI
+
+struct ButtonPrac: View {
+    var body: some View {
+        VStack{
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                
+            }
+        }
+    }
+}
+
+struct ButtonPrac_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonPrac()
+    }
+}
