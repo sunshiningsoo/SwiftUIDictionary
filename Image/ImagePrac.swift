448718d446c365f5466bae9cf85378dba512af61
parksungsoo <72736657+sunshiningsoo@users.noreply.github.com>
2022-04-17 12:56:34 +0900

    image upload

---
+++ Image/ImagePrac.swift
@@ -0,0 +1,25 @@
+//
+//  ImagePrac.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/16.
+//
+
+import SwiftUI
+
+struct ImagePrac: View {
+    var body: some View {
+        Image("flower")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .shadow(color: .black, radius: 20)
+            .overlay(Circle().stroke(Color.blue, lineWidth: 5))
+    }
+}
+
+struct ImagePrac_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePrac()
+    }
+}
