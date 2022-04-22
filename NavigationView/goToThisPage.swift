ed967352f44dd294f10bbcc7bcfbaf1ca8af1583
sunshiningsoo <yenkee1219@gmail.com>
2022-04-22 14:16:19 +0900

    navigation

---
+++ NavigationView/goToThisPage.swift
@@ -0,0 +1,20 @@
+//
+//  goToThisPage.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/22.
+//
+
+import SwiftUI
+
+struct goToThisPage: View {
+    var body: some View {
+        Text("This is goToThisPage")
+    }
+}
+
+struct goToThisPage_Previews: PreviewProvider {
+    static var previews: some View {
+        goToThisPage()
+    }
+}
