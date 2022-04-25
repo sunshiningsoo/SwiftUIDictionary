164b5c430ef861e5b14b85ad7e3a3de445029ace
sunshiningsoo <yenkee1219@gmail.com>
2022-04-25 14:32:29 +0900

    Create Three3dPrac.swift

---
+++ 3D/Three3dPrac.swift
@@ -0,0 +1,42 @@
+//
+//  Three3dPrac.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/24.
+//
+
+import SwiftUI
+
+struct Three3dPrac: View {
+    @State private var degrees = 0.0
+    @State private var rotateX = true
+    @State private var rotateY = true
+    @State private var rotateZ = true
+    
+    var body: some View {
+        
+        VStack(spacing: 30) {
+            Toggle("Rotate X-axis", isOn: $rotateX)
+            Toggle("Rotate Y-axis", isOn: $rotateY)
+            Toggle("Rotate Z-axis", isOn: $rotateZ)
+            Button("Hello World") {
+                withAnimation(.easeIn(duration: 1)) {
+                    self.degrees += 360
+                }
+            }
+            .padding(20)
+            .background(Color.red)
+            .foregroundColor(Color.white)
+            .rotation3DEffect(.degrees(degrees), axis: (x: rotateX ? 1 : 0, y: rotateY ? 1 : 0, z: rotateZ ? 1 : 0))
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ThreedPrac_Previews: PreviewProvider {
+    static var previews: some View {
+        Three3dPrac()
+    }
+}
