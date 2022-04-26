1c232bd64ce16a13a22d20557b19c50247a532d8
sunshiningsoo <yenkee1219@gmail.com>
2022-04-26 17:26:01 +0900

    Date타입을 String타입으로 바꿔 출력해보기

---
+++ DatePicker/AboutDateType.swift
@@ -0,0 +1,36 @@
+//
+//  AboutDateType.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct AboutDateType: View {
+    var currentDate:Date = Date()
+    @State var changeString:String = ""
+    
+    var body: some View {
+        ScrollView {
+            Text("\(currentDate)")
+            
+            Button("날짜 보내기"){
+                changeString = dateFormatterChange(changeOne: currentDate)
+                print(changeString)
+            }
+        }
+    }
+}
+
+func dateFormatterChange(changeOne : Date) -> String{
+    var dateFormatter:DateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yy년도 MM월 dd일 입니다"
+    return dateFormatter.string(from: changeOne)
+}
+
+struct AboutDateType_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutDateType()
+    }
+}
