1eb9ed36b2c6b7d960a4a04bfe5b533662214bce
sunshiningsoo <yenkee1219@gmail.com>
2022-04-29 14:45:59 +0900

    Create PickerPrac.swift

---
+++ Picker/PickerPrac.swift
@@ -0,0 +1,60 @@
+//
+//  PickerPrac.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/04/29.
+//
+
+import SwiftUI
+
+struct PickerPrac: View {
+    @State var isNow:String = "Red"
+    var colorList:[String] = ["Red", "Blue"]
+    
+    var body: some View {
+        VStack(spacing:30) {
+            Picker("pick one thing", selection: $isNow){
+                ForEach(colorList, id:\.self){
+                    Text($0)
+                }
+            }
+            .pickerStyle(.segmented)
+            Divider()
+            
+            Picker("pick one thing", selection: $isNow){
+                ForEach(colorList, id:\.self){
+                    Text($0)
+                }
+            }
+            Divider()
+            
+            Picker("pick one thing", selection: $isNow){
+                ForEach(colorList, id:\.self){
+                    Text($0)
+                }
+            }.pickerStyle(.inline)
+            Divider()
+            
+            Picker("pick one thing", selection: $isNow){
+                ForEach(colorList, id:\.self){
+                    Text($0)
+                }
+            }.pickerStyle(.menu)
+            Divider()
+            
+            Picker("pick one thing", selection: $isNow){
+                ForEach(colorList, id:\.self){
+                    Text($0)
+                }
+            }.pickerStyle(.wheel)
+        
+        }
+        
+    }
+}
+
+struct SegmentPickerPrac_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerPrac()
+    }
+}
