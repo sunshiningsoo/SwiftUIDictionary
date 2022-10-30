e62ba2669661e6f2042ef78ed0fed167ea978bda
sunshiningsoo <yenkee1219@gmail.com>
2022-10-30 21:37:18 +0900

    [Add] combine subscription 정리

---
+++ SwiftUIDictionary/Combine/Subscribtion.swift
@@ -0,0 +1,54 @@
+//
+//  Subscribtion.swift
+//  SwiftUIDictionary
+//
+//  Created by 박성수 on 2022/10/30.
+//
+
+import SwiftUI
+import Combine
+
+class SubscribtionViewModel: ObservableObject {
+    @Published var timer: AnyCancellable? = nil
+    @Published var num: Int = 0
+    
+    init() {
+        timerSetting()
+    }
+    
+    func timerSetting() {
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .scan(0, { (count, _) in
+                return count + 1
+            })
+            .filter({ count in
+                return count < 3
+            })
+            // filter에서 1차적으로 Auth같은 것을 걸러줌 그러고 나서 무거운 작업같은 서버통신을 sink에 넣어준다
+            .sink(receiveCompletion: { completion in
+                print("receive Completion: \(completion)")
+            }, receiveValue: { receiveValue in
+                print("receiveValue: \(receiveValue)")
+                self.num = receiveValue
+            })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.timer?.cancel()
+        }
+    }
+}
+
+struct Subscribtion: View {
+    @StateObject var vm = SubscribtionViewModel()
+    
+    var body: some View {
+        Text("\(vm.num)")
+    }
+}
+
+struct Subscribtion_Previews: PreviewProvider {
+    static var previews: some View {
+        Subscribtion()
+    }
+}
