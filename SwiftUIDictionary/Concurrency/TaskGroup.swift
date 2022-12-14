23c5ab9ee7307e2f45982b8b32a95b9096cc5774
sunshiningsoo <yenkee1219@gmail.com>
2022-12-14 23:31:55 +0900

    [Enhancement] TaskGroup 주석 설명 추가

--- SwiftUIDictionary/Concurrency/TaskGroup.swift
+++ SwiftUIDictionary/Concurrency/TaskGroup.swift
@@ -36,17 +36,18 @@ class TaskGroupDataManager {
         
         return try await withThrowingTaskGroup(of: UIImage?.self) { group in
             var images: [UIImage] = []
-            // 컴파일러에게 우리는 아니까 어느정도로 크기를 잡아 놓으라고 지정해주는 것 어플리케이션의 속도 boost됨
+            // 컴파일러에게 개발을 하는 사람은 그 크기를 아니까 어느정도로 메모리 공간을 잡아 놓으라고 지정해주는 것 어플리케이션의 속도 boost됨
             images.reserveCapacity(urlStrings.count)
             
             for urlString in urlStrings {
                 group.addTask {
-                    // 50개 요청하면 마지막 것이 취소 되었다 생각하면, 다 안뜨기 때문에 try?를 사용해서 에러처리 해주는 것이 좋음
-                    // UIImage도 옵셔널 값으로 바뀌어야 함
+                    // 50개 요청해서 중간 몇개가 취소되었다고, nil로 나가버리면 이미지가 다 안뜨기 때문에 try?를 사용해서 에러처리 해주는 것이 좋음
+                    // 위의 타입에서 UIImage도 옵셔널 값으로 바뀌어야 함
                     try? await self.fetchImage(urlString: urlString)
                 }
             }
             
+            // 아래와 같이 group에 addTask를 일일히 해줄 수 있지만, 상단처럼 Array로 다뤄주는 것이 더 편함
 //            group.addTask {
 //                try await self.fetchImage(urlString: self.url)
 //            }
